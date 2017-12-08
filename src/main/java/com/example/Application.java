b4cb3e0820943395141c5ad7a86c1d39c4a2fc58
mtsachakis <mtsachakis@telensa.com>
2017-12-08 17:01:27 +0000

    initial commit

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * See https://spring.io/guides/gs/batch-processing/
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
